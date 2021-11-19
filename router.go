c3063b4b81f89d28754b08e8b5f6ce09ac492bc8
Cesar <c.caycho46@gmail.com>
2021-11-18 19:30:37 -0500

    complete basic crud

--- router.go
+++ router.go
@@ -12,17 +12,21 @@ func setupRouter() (r *gin.Engine) {
 	r.Use(static.Serve("/", static.LocalFile("./statics", true)))
 
 	s := r.Group("/api/v1")
+	s.GET("/users", handler.GetAllUsers)
 	{
 		getuserFromBody := s.Group("/")
 		getuserFromBody.Use(middleware.GetUserFromBody)
 		{
-			getuserFromBody.GET("/", handler.SignUp)
+			getuserFromBody.POST("/signin", handler.SignIn)
+			getuserFromBody.POST("/signup", handler.SignUp)
 		}
 
 		getuserFromToken := s.Group("/")
 		getuserFromToken.Use(middleware.GetUserFromToken)
 		{
-			// getuserFromToken.GET("/", nil)
+			getuserFromToken.GET("/logout", handler.Profile)
+			getuserFromToken.DELETE("/logout", handler.DeleteUser)
+			getuserFromToken.HEAD("/logout", handler.LogOut)
 		}
 	}
 	return
